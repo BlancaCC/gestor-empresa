4b00025e14d24d9f7fc14e3c55fa952f02ed9ce2
danikc <danielkc@correo.ugr.es>
2021-01-14 19:38:07 +0100

    añado restricciones, parte de almacen creo que terminada

--- html/almacen/server/borrarComponentes.php
+++ html/almacen/server/borrarComponentes.php
@@ -2,28 +2,34 @@
 	include('cabecera.php');
 
 
-
-    // -- lectura datos formulario ---
-
-        // variables no nulas
     $idComp= $_POST['idComp'];
 
+		$sql = "SELECT * FROM almacenadoEn WHERE idComp='$idComp'";
 
-    $sql = "DELETE FROM componente WHERE idComp='$idComp';";
+		$query = mysqli_query($dbconn,$sql)
+		 or die (mysqli_error($dbconn));
 
+		 $return ="";
 
-    $return ="";
-
-    	if (mysqli_query($dbconn, $sql)) {
-    		$return .=  "Se han borrado las componentes con éxito.";
-
-    	}
-    	else {
-    		$return .= "Error: " . $sql . "" . mysqli_error($dbconn);
-         }
+		if(mysqli_num_rows($query)==0)
+		{
+	    $sql = "DELETE FROM componente WHERE idComp='$idComp';";
 
 
+	    $return ="";
 
+	    	if (mysqli_query($dbconn, $sql)) {
+	    		$return .=  "Se han borrado las componentes con éxito.";
+
+	    	}
+	    	else {
+	    		$return .= "Error: " . $sql . "" . mysqli_error($dbconn);
+	         }
+		 }
+		 else
+		 {
+				$return .= "Hay que dar de baja primero la componente en las estanterías";
+		 }
 
 
     echo $return;
